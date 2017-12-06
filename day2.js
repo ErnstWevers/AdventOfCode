d3e5c7409131f024ac52389ae519de8c22be497e
ErnstWevers <ernstwevers@gmail.com>
2017-12-06 17:44:53 +0100

    fixed splitting

--- day2.js
+++ day2.js
@@ -6,43 +6,19 @@ readStream.on('data', function(chunk) {
   inputText+= chunk;
 })
 readStream.on('end', function() {
-  console.log(checkSum(prepareArray(inputText)))
+  console.log(sumDiff(prepareArray(inputText)))
 })
 
 function prepareArray(numberInput){
-  let numberArray = numberInput.split("\t").map(function (number) {
-    return parseInt(number,10);
-  });
-
-  //very ugly because splitting by line AND tab is weird :(
-  table = [];
-  for(let i = 0; i < 16; i++){
-    tempRow = [];
-    for(let j = 0; j < 16; j++){
-      tempRow.push(numberArray[i*j+j])
-    }
-    table.push(tempRow)
-  }
-  return table;
+  return numberInput.split("\n").map(row => row.split("\t"))
 }
 
-function findDiff(array){
-  let min = 9999;
-  let max = 0;
-
-  array.map((entry) => {
-    min = entry < min ? entry : min
-    max = entry > max ? entry : max
-  })
-  console.log(max-min)
-  return (max - min)
+function findDiff(row){
+  return (Math.max.apply(null, row) - Math.min.apply(null, row))
 }
 
-function checkSum(table){
+function sumDiff(array){
   let sum = 0;
-  console.log(findDiff(table[15]))
-  for(let i = 0; i < 16; i++){
-    sum += findDiff(table[i])
-  }
+  array.map( row => { sum += findDiff(row)} )
   return sum;
 }
