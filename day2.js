fbcbc24f5c681b48e30cee4e6bab419747466bad
ErnstWevers <ernstwevers@gmail.com>
2017-12-06 18:42:02 +0100

    it works but needs a switch

--- day2.js
+++ day2.js
@@ -7,6 +7,7 @@ readStream.on('data', function(chunk) {
 })
 readStream.on('end', function() {
   console.log(sumDiff(prepareArray(inputText)))
+  console.log(sumDivide(prepareArray(inputText)))
 })
 
 function prepareArray(numberInput){
@@ -18,7 +19,33 @@ function findDiff(row){
 }
 
 function sumDiff(array){
-  let sum = 0;
-  array.map( row => { sum += findDiff(row)} )
-  return sum;
+  let sum = 0
+  array.map( row => { sum += findDiff(row) })
+  return sum
+}
+
+function sumDivide(array){
+  let sum = 0
+  array.map( row => { sum += evenDivide(row) })
+  return sum
+}
+
+function evenDivide(row){
+  var sum = 0;
+  number = row.pop()
+  console.log("I am here")
+  row.map( entry => {
+    if(entry%number === 0 || number%entry === 0){
+      console.log(entry +" " + number)
+      //switch here
+      sum = entry > number ? sum+entry/number : sum
+      sum = number > entry ? sum+number/entry : sum
+      sum = number === entry ? sum+1 : sum
+      console.log(sum)
+    }
+  })
+  if(row.length > 1){
+    evenDivide(row)
+  }
+  return sum
 }
