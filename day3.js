06297cfbd7ea286a889e6f0e00a6c2985cef50ab
ErnstWevers <ernstwevers@gmail.com>
2017-12-08 17:00:48 +0100

    now with working numbers

--- day3.js
+++ day3.js
@@ -1,7 +1,7 @@
-const targetSquare = 48;
+const targetSquare = 347991;
 
 function findOuterSquare(n){
-  let x = box = 1;
+  let x = box = 0;
   while(n > box**2){
     x++
     box = (2*x+1)
@@ -14,16 +14,16 @@ function findRemain(n){
 }
 
 function findFromCorner(n){
-  return -(findRemain(n)%(findOuterSquare(n)))
+  return -(findRemain(n)%(findOuterSquare(n)-1))
 }
 
 function findSteps(n){
   colStep = (findOuterSquare(n)-1)/2
   fromCor = findFromCorner(n)
+  console.log("something: " + fromCor)
   middleSq = (colStep + 1)
   rowStep = middleSq < fromCor ? fromCor - middleSq : middleSq - fromCor
   return(colStep + rowStep - 1)
 }
-
 
 console.log(findSteps(targetSquare))
