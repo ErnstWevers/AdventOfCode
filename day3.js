625dd5d45c46baa10f9ccb108bc25cbbaab499c8
ErnstWevers <ernstwevers@gmail.com>
2017-12-09 22:15:15 +0100

    making the spiral, add the value

--- day3.js
+++ day3.js
@@ -26,4 +26,45 @@ function findSteps(n){
   return(colStep + rowStep - 1)
 }
 
-console.log(findSteps(targetSquare))
+// console.log(findSteps())
+
+function populateSpiral(n){
+  spiral = [{'x': 0, 'y' : 0, 'v':0}]
+  //array of arrays of the form [x, y, value]
+  //spiral follows the form: n right, n up, n+1 left, n+1 down, repeat
+  //value is mapped to the array
+
+  for(let i = 1 ; i < 3 ; i++){
+    //move right
+    for(let a = 0 ; a < i ; a++){
+      let nextBlock = Object.assign({},spiral[spiral.length-1])
+      nextBlock.x++
+      spiral.push(nextBlock)
+    }
+    //move up
+    for(let a = 0 ; a < i ; a++){
+      let nextBlock = Object.assign({},spiral[spiral.length-1])
+      nextBlock.y++
+      spiral.push(nextBlock)
+    }
+    //move left
+    for(let a = 0 ; a <= i ; a++){
+      let nextBlock = Object.assign({},spiral[spiral.length-1])
+      nextBlock.x--
+      spiral.push(nextBlock)
+    }
+    //move down
+    for(let a = 0 ; a <= i ; a++){
+      let nextBlock = Object.assign({},spiral[spiral.length-1])
+      nextBlock.y--
+      spiral.push(nextBlock)
+    }
+  }
+  return spiral
+}
+
+function populateValue(x,y){
+  //the value is the sum of the values where x and y don't deviate more than one
+}
+
+console.log(populateSpiral())
