73467f8825801bdd50d68661528d3b06304bd11a
ErnstWevers <ernstwevers@gmail.com>
2017-12-10 19:13:06 +0100

    I have taken steps

---
+++ day5.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var readStream = fs.createReadStream('./dataInput/day5.txt');
+var input = [];
+
+readStream.on('data', function(chunk) {
+  input+= chunk;
+});
+
+readStream.on('end', function() {
+  // console.log(checkAll(prepareArray(input)))
+  console.log(takeSteps(prepareArray(input)))
+});
+
+function prepareArray(input){
+  return input.split("\n").map(number => parseInt(number))
+}
+
+function takeSteps(steps){
+  var numberOfSteps = 0,
+      currentPos = 0,
+      stepSize = 0;
+
+  //terminate when currentPos > length
+  while(currentPos<steps.length || currentPos < 0){
+    stepSize = steps[currentPos]
+    steps[currentPos]++
+    currentPos += stepSize
+    numberOfSteps++
+  }
+
+  return numberOfSteps
+}
