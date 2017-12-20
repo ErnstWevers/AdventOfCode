76ff5fc92b193ab9732b102ffb0b7fcf5b900c3a
ErnstWevers <ernstwevers@gmail.com>
2017-12-20 11:45:27 +0100

    parsing that input correctly

--- day8.js
+++ day8.js
@@ -8,10 +8,19 @@ readStream.on('data', function(chunk) {
 });
 
 readStream.on('end', function() {
-  console.log(parseInput(input))
+  console.log(simplifyAdd(parseInput(input)))
+  // parseInput(input)
 });
 
 function parseInput(input){
   //parses the input and returns a sorted collection of parent/child relationships
-  return input.split('\n').map(row => row.split(' if '))
+  return input.split('\n').map(row => row.split(' if ')).map(entry => entry[0].split(' ').concat(entry[1]))
+}
+
+function simplifyAdd(input){
+  return input.map(row => {
+    if(row[1] === 'dec') {row[2] = -row[2]}
+    row.splice(1,1)
+    return row
+    })
 }
