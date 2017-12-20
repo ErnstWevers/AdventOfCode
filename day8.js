faca01dbcaae883b8bcb7fc03ae224659f63dacc
ErnstWevers <ernstwevers@gmail.com>
2017-12-20 14:12:18 +0100

    now working

--- day8.js
+++ day8.js
@@ -1,22 +1,23 @@
 var fs = require('fs'),
     readStream = fs.createReadStream('./dataInput/day8.txt'),
     input = [],
-    tower = [];
+    register = [];
 
 readStream.on('data', function(chunk) {
   input+= chunk;
 });
 
 readStream.on('end', function() {
-  console.log(parseInput(input))
-  // parseInput(input)
+  evaluateInput(parseInput(input))
+  // console.log(parseInput(input))
+  console.log(sortResults(register))
 });
 
 function parseInput(input){
   //parses the input and returns a sorted collection of parent/child relationships
   let parsed =  input.split('\n').map(row => row.split(' if ')).map(entry => entry[0].split(' ').concat(entry[1]))
   simplifyToAdd(parsed)
-  return extractSecond(parsed)
+  return fixFunction(parsed)
 }
 
 function simplifyToAdd(input){
@@ -24,22 +25,38 @@ function simplifyToAdd(input){
   return input.map(row => {
     if(row[1] === 'dec') {row[2] = -row[2]}
     row.splice(1,1)
-    return row
   })
 }
 
-function extractSecond(input){
+function fixFunction(input){
   return input.map(row => {
-    let second = row[2].split(' ')[0]
-    return row.concat(second)
+    let one = row[2].split(' ')[0]
+    let two = row[2].split(' ').slice(1,3).join(' ')
+    return {eval: one, targ: row[0], add: parseInt(row[1]), func:two}
+  })
+}
+
+function evaluateInput(input){
+  input.map(entry => {
+    checkExist(entry)
+    let toCheck = register.find(item => item.name === entry.eval)
+    let toEdit = register.find(item => item.name === entry.targ)
+    if(eval(`${toCheck.value} ${entry.func}`)){
+      toEdit.value += entry.add
+    }
+  })
+}
+
+function checkExist(entry){
+  if(typeof (register.find(item => item.name === entry.eval)) === 'undefined') {register.push({name: entry.eval, value:0})}
+  if(typeof (register.find(item => item.name === entry.targ)) === 'undefined') {register.push({name: entry.targ, value:0})}
+}
+
+function sortResults(input){
+  input.sort((i,j) => {
+    if(i.value > j.value){ return -1}
+    if(i.value < j.value){ return 1}
+    return 0
   })
   return input
-}
-
-function evaluateEntry(entry){
-  //check whether both entries exist
-}
-
-function checkExist(register){
-
 }
